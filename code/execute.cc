abdfd9041ff5dbf5489219e118228b3abd7d0746
nlmartin <nlmartin@303x27.csc.calpoly.edu>
2014-02-20 16:12:57 -0800

    Added case statements for each operation we will be implementing.

--- code/execute.cc
+++ code/execute.cc
@@ -26,6 +26,39 @@ void execute() {
     case SP_SLL:
       rf.write(rt.rd, rf[rt.rt] << rt.sa);
       break;
+    // Our modifications start here
+    //
+    // Freebie from the ppt slides:
+    case SP_ADDU:
+      rf.write(rt.rd, rf[rt.rs] + rf[rt.rt]);
+      break;
+    case SP_SLT:
+      break;
+    case SP_JR:
+      break;
+    case SP_SLL:
+      break;
+    case SP_SRL:
+      break;
+    case SP_SRA:
+      break;
+    case SP_ADD:
+      break;
+    case SP_SUB:
+      break;
+    case SP_SUBU:
+      break;
+    case SP_AND:
+      break;
+    case SP_OR:
+      break;
+    case SP_XOR:
+      break;
+    case SP_NOR:
+      break;
+    case SP_JALR:
+      break;
+    // Our modifications end here
     default:
       cout << "Unsupported instruction: ";
       instr.printI(instr);
@@ -36,6 +69,36 @@ void execute() {
   case OP_ADDIU:
     rf.write(ri.rt, rf[ri.rs] + signExtend16to32ui(ri.imm));
     break;
+  // Our modifications start here
+  case OP_ORI:
+    break;
+  case OP_SB:
+    break;
+  case OP_LW:
+    break;
+  case OP_LBU:
+    break;
+  case OP_LB:
+    break;
+  case OP_SLTI:
+    break;
+  case OP_SLTIU:
+    break;
+  case OP_BNE:
+    break;
+  case OP_BEQ:
+    break;
+  case OP_BLEZ:
+    break;
+  case OP_LUI:
+    break;
+  case OP_JAL:
+    break;
+  case OP_J:
+    break;
+  case OP_LUI:
+    break;
+  // Our modifications end here
   case OP_SW:
     addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
     caches.access(addr);
